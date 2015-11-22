d61ce4006d1fcd0aa81e1b6e9c7981ee36c16cce
Hugh Kennedy <hughskennedy@gmail.com>
2015-11-23 01:16:28 +1000

    terrain

--- terrain.js
+++ terrain.js
@@ -9,6 +9,7 @@ export default function terrain (x, y, z) {
 
   return (
     // noise.noise3D(x, y, z) +
-    noise.noise3D(x * 0.5, y * 0.5, z * 0.5)
+    noise.noise3D(x * 0.5, y * 0.5, z * 0.5) +
+    noise.noise3D(x * 4.1, y * 4.1, z * 4.1) * 0.15
   ) - 0.3 - Math.max(0, y * 3)
 }
